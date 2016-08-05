eb7b1cf4b41231cc2c89a8bc3a0a446e24fa85c1
zhangycz <a286336276@hotmail.com>
2016-08-05 23:49:00 +0800

    分理出修改种子文件

---
+++ Chun.Demo.VIEW/ItemForm/AlterTorrentByInnerName.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Chun.Demo.AnalyzeTorrent;
+
+namespace Chun.Demo.VIEW
+{
+    public partial class AlterTorrentByInnerName : Form
+    {
+        object _locker;
+
+        private List<Torrent> tlList { get; set; } = new List<Torrent>();
+        
+
+        public AlterTorrentByInnerName()
+        {
+            InitializeComponent();
+        }
+        public AlterTorrentByInnerName(string[] filePaths)
+        {
+            InitializeComponent();
+            try
+            {
+                foreach (var item in filePaths)
+                {
+                    var tor = new Torrent(item);
+                    tlList.Add(tor);
+                }
+
+                torrentBindingSource.DataSource = tlList;
+                //Parallel.ForEach(filePaths, item =>
+                //{
+
+                //    if (Tool.ChangFileName(item, @"C:\Users\a2863\Desktop\种子", ".TORRENT"))
+                //    {
+                //        lock (Locker)
+                //            if (currentCount < maxCount - loseCount)
+                //                currentCount++;
+                //    }
+                //    else
+                //    {
+                //        lock (Locker)
+                //            loseCount++;
+                //    }
+                //});
+                //this.Invoke(new MethodInvoker(( ) => MessageBox.Show("successful")));
+            }
+            catch (Exception ex)
+            {
+               // Invoke(new MethodInvoker(() => MessageBox.Show(Resources.MainForm_打开文件ToolStripMenuItem_Click_)));
+            }
+        }
+
+        public object Locker
+        {
+            get
+            {
+                return _locker;
+            }
+
+            set
+            {
+                _locker = value;
+            }
+        }
+    }
+}
