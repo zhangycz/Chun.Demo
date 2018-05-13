bd59fcdbbb048e51f2e1abfb9f027ff64de88dda
Ocun <zychun@hotmail.com>
2018-05-13 11:59:16 +0800

    11
    11

---
+++ Chun.Demo.Common/Tool/HardwareTools.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Management;
+using Digiwin.Chun.Common.Model;
+
+namespace Digiwin.Chun.Common.Controller {
+    /// <summary>
+    ///     获取系统硬件信息
+    /// </summary>
+    public static class HardwareTools {
+        /// <summary>
+        /// </summary>
+        public static HardwareEntity GetHardwareInfo() {
+            HardwareEntity hardwareInfo = null;
+            try {
+                var cpuInfo = GetCpuInfo();
+                var mainBoardInfo = GetMainBoardInfo();
+                var diskDriveInfo = GetDiskDriveInfo();
+                var networkInfo = GetNetworkInfo();
+                var osInfo = GetOsInfo();
+                var memoryInfo = GetMemoryInfo();
+                hardwareInfo = new HardwareEntity
+                {
+                    CpuInfos = cpuInfo,
+                    MainBoardInfos = mainBoardInfo,
+                    DiskDriveInfos = diskDriveInfo,
+                    NetworkInfos = networkInfo,
+                    OsInfo = osInfo,
+                    MemoryInfo = memoryInfo
+                };
+
+            }
+            catch (Exception ex) {
+                LogTools.LogError($"GetHardwareInfo error! detail {ex.Message}");
+            }
+            return hardwareInfo;
+        }
+
+        /// <summary>
+        ///     獲取硬件信息集合
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public static ManagementObjectCollection GetManagementObjectCollection(string model) {
+            var mc = new ManagementClass(model);
+            return mc.GetInstances();
+        }
+
+        /// <summary>
+        ///     Cpu信息
+        /// </summary>
+        /// <returns></returns>
+        public static List<CpuInfo> GetCpuInfo() {
+            var cpuInfos = new List<CpuInfo>();
+            try {
+                var moc = GetManagementObjectCollection(WMIPath.Win32_Processor.ToString());
+                var empStr = string.Empty;
+                foreach (var mo in moc) {
+                    var cpuInfo = new CpuInfo {
+                        ProcessorId = (mo.Properties["ProcessorId"].Value ?? empStr).ToString(),
+                        Name = (mo.Properties["Name"].Value ?? empStr).ToString(),
+                        Status = (mo.Properties["Status"].Value ?? empStr).ToString(),
+                        SystemName = (mo.Properties["SystemName"].Value ?? empStr).ToString()
+                    };
+                    cpuInfos.Add(cpuInfo);
+                }
+            }
+            catch (Exception ex) {
+                LogTools.LogError($"GetCpuInfo Error! Detail:{ex.Message}");
+            }
+            return cpuInfos;
+        }
+
+        /// <summary>
+        ///     主板信息
+        /// </summary>
+        public static List<MainBoardInfo> GetMainBoardInfo() {
+            var mainBoardInfos = new List<MainBoardInfo>();
+            try {
+                var moc = GetManagementObjectCollection(WMIPath.Win32_BaseBoard.ToString());
+                var empStr = string.Empty;
+                foreach (var mo in moc) {
+                    var serialNumber = (mo.Properties["SerialNumber"].Value ?? empStr).ToString();
+                    var manufacturer = (mo.Properties["Manufacturer"].Value ?? empStr).ToString();
+                    var product = (mo.Properties["Product"].Value ?? empStr).ToString();
+                    var version = (mo.Properties["Version"].Value ?? empStr).ToString();
+                    var mainBoardInfo = new MainBoardInfo {
+                        SerialNumber = serialNumber,
+                        Manufacturer = manufacturer,
+                        Product = product,
+                        Version = version
+                    };
+                    mainBoardInfos.Add(mainBoardInfo);
+                }
+            }
+            catch (Exception ex) {
+                LogTools.LogError($"GetMainBoardInfo Error! Detail:{ex.Message}");
+            }
+            return mainBoardInfos;
+        }
+
+        /// <summary>
+        ///     硬盘信息
+        /// </summary>
+        public static List<DiskDriveInfo> GetDiskDriveInfo() {
+            var diskDriverInfos = new List<DiskDriveInfo>();
+            try {
+                var empStr = string.Empty;
+                var moc = GetManagementObjectCollection(WMIPath.Win32_DiskDrive.ToString());
+                long gb = 1024 * 1024 * 1024;
+                foreach (var mo in moc) {
+                    var size = Convert.ToDouble(mo.Properties["Size"].Value ?? 0) / gb;
+                    //var freeSpace = Convert.ToDouble(mo["FreeSpace"]?? 0) / gb;
+                    var diskDriverInfo = new DiskDriveInfo {
+                        SerialNumber = (mo.Properties["SerialNumber"].Value ?? empStr).ToString(),
+                        Model = (mo.Properties["Model"].Value ?? empStr).ToString(),
+                        Size = size.ToString("#"),
+                        //FreeSpace = freeSpace.ToString("#"),
+                        //UsedSpace = (size -
+                        //            freeSpace).ToString("#")
+                    };
+                    diskDriverInfos.Add(diskDriverInfo);
+                }
+            }
+            catch (Exception ex) {
+                LogTools.LogError($"GetDiskDriveInfo Error! Detail:{ex.Message}");
+            }
+            return diskDriverInfos;
+        }
+
+        /// <summary>
+        ///     网络连接信息
+        /// </summary>
+        public static List<NetworkInfo> GetNetworkInfo() {
+            var networkInfos = new List<NetworkInfo>();
+            try {
+                var empStr = string.Empty;
+                var moc = GetManagementObjectCollection(WMIPath.Win32_NetworkAdapterConfiguration.ToString());
+                foreach (var mo in moc) {
+                    if (!(bool) mo["IPEnabled"])
+                        continue;
+                    var ipAr = (Array) mo.Properties["IpAddress"].Value;
+                    var subnets = (string[]) mo["IPSubnet"]; //子网掩码
+                    var gateways = (string[]) mo["DefaultIPGateway"]; //网关
+                    var dnses = (string[]) mo["DNSServerSearchOrder"]; //DNS
+                    var description = mo["Description"].ToString();
+                    var ipAddress = ipAr.GetValue(0).ToString();
+                    var ipSubnet = subnets?.GetValue(0).ToString() ?? empStr;
+                    var defaultIpGateway = gateways?.GetValue(0).ToString() ?? empStr;
+                    var dnsServerSearchOrder = dnses?.GetValue(0).ToString() ?? empStr;
+                    var networkInfo = new NetworkInfo {
+                        MacAddress = (mo.Properties["MACAddress"].Value ?? empStr).ToString(),
+                        IpAddress = ipAddress,
+                        Caption = mo.Properties["Caption"].Value.ToString(),
+                        IPSubnet = ipSubnet,
+                        DefaultIPGateway = defaultIpGateway,
+                        DNSServerSearchOrder = dnsServerSearchOrder,
+                        Description = description
+                    };
+                    networkInfos.Add(networkInfo);
+                }
+            }
+            catch (Exception ex) {
+                LogTools.LogError($"GetNetworkInfo Error! Detail:{ex.Message}");
+            }
+            return networkInfos;
+        }
+
+        /// <summary>
+        ///     操作系统信息
+        /// </summary>
+        public static List<OsInfo> GetOsInfo() {
+            var osInfos = new List<OsInfo>();
+            try {
+                var empStr = string.Empty;
+                var moc = GetManagementObjectCollection(WMIPath.Win32_OperatingSystem.ToString());
+                foreach (var mo in moc) {
+                    var osInfo = new OsInfo {
+                        Name = (mo.Properties["Name"].Value ?? empStr).ToString(),
+                        Version = (mo.Properties["Version"].Value ?? empStr).ToString(),
+                        SystemDirectory = (mo.Properties["SystemDirectory"].Value ?? empStr).ToString()
+                    };
+                    osInfos.Add(osInfo);
+                }
+            }
+            catch (Exception ex) {
+                LogTools.LogError($"GetOsInfo Error! Detail:{ex.Message}");
+            }
+            return osInfos;
+        } 
+        /// <summary>
+        ///     内存信息
+        /// </summary>
+        public static List<MemoryInfo> GetMemoryInfo() {
+            var memoryInfos = new List<MemoryInfo>();
+            try {
+                var empStr = string.Empty;
+                var moc = GetManagementObjectCollection(WMIPath.Win32_PhysicalMemory.ToString());
+                foreach (var mo in moc) {
+                    var memoryInfo = new MemoryInfo
+                    {
+                        // ReSharper disable once PossibleLossOfFraction
+                        Size = Math.Round(Int64.Parse(mo.Properties["Capacity"].Value.ToString())
+                        / 1024 / 1024 / 1024.0, 1).ToString(CultureInfo.InvariantCulture),
+                        Name = (mo.Properties["Name"].Value ?? empStr).ToString(),
+                        Version = (mo.Properties["Version"].Value ?? empStr).ToString(),
+                        Manufacturer = (mo.Properties["Manufacturer"].Value ?? empStr).ToString(),
+                        Model = (mo.Properties["Model"].Value ?? empStr).ToString(),
+                        MemoryType = (mo.Properties["MemoryType"].Value ?? empStr).ToString(),
+                        Speed = (mo.Properties["Speed"].Value ?? empStr).ToString(),
+                    };
+                    memoryInfos.Add(memoryInfo);
+                }
+            }
+            catch (Exception ex) {
+                LogTools.LogError($"GetOsInfo Error! Detail:{ex.Message}");
+            }
+            return memoryInfos;
+        }
+    }
+}
---
+++ Chun.Demo.Common/Tool/LogTools.cs
@@ -0,0 +1,191 @@
+﻿// create By 08628 20180411
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Digiwin.Chun.Common.Model;
+using Digiwin.Chun.Common.Views;
+
+namespace Digiwin.Chun.Common.Controller {
+    /// <summary>
+    /// 日志
+    /// </summary>
+    public static class LogTools {
+        #region 日志
+
+     
+        /// <summary>
+        ///     日志
+        /// </summary>
+        public static void WriteLogByTreeView(MyTreeView treeView) {
+            var toolpars = MyTools.Toolpars;
+            var pathDic = MyTools.GetTreeViewFilePath(treeView.Nodes);
+            var operationLog = (toolpars.CustomerName == null || toolpars.CustomerName.Equals(string.Empty)
+                ? DateTime.Now.ToString("yyyyMMddhhmmss")
+                : toolpars.CustomerName);
+            var logPath=GetLogDir(operationLog);
+
+            var logStr = new StringBuilder();
+           const string empStr = @"      ";
+            foreach (var kv in pathDic)
+            {
+                foreach (var fileinfo in kv.Value)
+                logStr.AppendLine($"{(logStr.Length>0?empStr:string.Empty)}# {kv.Key} {empStr}{fileinfo.FileName}");
+            }
+            LogMsg(logPath, logStr.ToString());
+        }
+
+        /// <summary>
+        /// 记录错误信息
+        /// </summary>
+        /// <param name="msg"></param>
+        public static void LogError(string msg)
+        {
+            var logPath = GetLogDir($@"error_{DateTime.Now:yyyyMMdd}");
+            LogMsg(logPath, msg);
+        }
+
+
+        /// <summary>
+        /// 记录日志
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="logPath"></param>
+        public static void LogMsg(string logPath, string msg ) {
+         
+            var logStr = new StringBuilder();
+            var headContentStr = GetHeadStr(@"_", 50);
+            var empStr = "      ";
+            if (!File.Exists(logPath)) {
+                logStr.AppendLine(GetHardInfo());
+            }
+            logStr.AppendLine($"{headContentStr}OperationMsg{headContentStr}");
+            logStr.AppendLine($"{empStr}#OperationTime#{empStr}{DateTime.Now:yyyy-MM-dd hh:mm:fff}")
+                .AppendLine($"{empStr}#OperationUser#{empStr}{Environment.MachineName}")
+                .AppendLine($"{empStr}#WinUser#{empStr}{Environment.UserName}")
+                .AppendLine($"{empStr}{msg}").AppendLine($"{headContentStr}OperationMsg{headContentStr}");
+
+            WriteToFile(logPath, logStr.ToString());
+        }
+
+        /// <summary>
+        /// 硬件信息
+        /// </summary>
+        /// <returns></returns>
+        public static string GetHardInfo() {
+            var hardwareInfo = MyTools.HardwareInfo;
+            var empStr = "      ";
+            var logStr = new StringBuilder();
+            var headContentStr = GetHeadStr(@"_", 50);
+            logStr.AppendLine($"{headContentStr}PCInfo{headContentStr}");
+            if (hardwareInfo != null) {
+                logStr.AppendLine($"{empStr}#CpuCount#{empStr}{hardwareInfo.CpuInfos?.Count}");
+
+                for (var i = 0; i < hardwareInfo.CpuInfos?.Count; i++)
+                {
+                    var cpuInfos = hardwareInfo.CpuInfos[i];
+
+                    logStr.AppendLine($"{empStr}#Manufacturer{i}#{empStr}{cpuInfos.Manufacturer}")
+                        .AppendLine($"{empStr}#CpuName{i}#{empStr}{cpuInfos.Name}")
+                        .AppendLine($"{empStr}#Speed#{empStr}{cpuInfos.MaxClockSpeed}");
+                }
+                for (var i = 0; i < hardwareInfo.OsInfo?.Count; i++)
+                {
+                    var osInfo = hardwareInfo.OsInfo[i];
+                    logStr.AppendLine($"{empStr}#OSName{i}#{empStr}{osInfo.Name}")
+                        .AppendLine($"{empStr}#OSVersion{i}#{empStr}{osInfo.Version}")
+                        .AppendLine($"{empStr}#WinverInfo#{empStr}{Environment.OSVersion.VersionString}")
+                        .AppendLine($"{empStr}#CurrentDomain#{empStr}{Environment.UserDomainName}");
+                }
+                for (var i = 0; i < hardwareInfo.NetworkInfos?.Count; i++)
+                {
+                    var netInfo = hardwareInfo.NetworkInfos[i];
+                    logStr.AppendLine($"{empStr}#Description#{empStr}{netInfo.Description}");
+                    logStr.AppendLine($"{empStr}#IpAddress{i}#{empStr}{netInfo.IpAddress}");
+                    logStr.AppendLine($"{empStr}#MacAddress{i}#{empStr}{netInfo.MacAddress}");
+                }
+
+                for (var i = 0; i < hardwareInfo.MemoryInfo?.Count; i++)
+                {
+                    var memoryInfos = hardwareInfo.MemoryInfo[i];
+                    logStr.AppendLine($"{empStr}#Manufacturer{i}#{empStr}{memoryInfos.Manufacturer}");
+                    logStr.AppendLine($"{empStr}#Size{i}#{empStr}{memoryInfos.Size}G");
+                    logStr.AppendLine($"{empStr}#Speed{i}#{empStr}{memoryInfos.Speed}MHz");
+                }
+                for (var i = 0; i < hardwareInfo.MainBoardInfos?.Count; i++)
+                {
+                    var mainBoardInfos = hardwareInfo.MainBoardInfos[i];
+                    logStr.AppendLine($"{empStr}#Manufacturer{i}#{empStr}{mainBoardInfos.Manufacturer}");
+                    logStr.AppendLine($"{empStr}#Product{i}#{empStr}{mainBoardInfos.Product}");
+                    logStr.AppendLine($"{empStr}#SerialNumber{i}#{empStr}{mainBoardInfos.SerialNumber}");
+                    logStr.AppendLine($"{empStr}#Version{i}#{empStr}{mainBoardInfos.Version}");
+                }
+                for (var i = 0; i < hardwareInfo.DiskDriveInfos?.Count; i++)
+                {
+                    var diskDriveInfo = hardwareInfo.DiskDriveInfos[i];
+                    logStr.AppendLine($"{empStr}#Model{i}#{empStr}{diskDriveInfo.Model}");
+                    logStr.AppendLine($"{empStr}#SerialNumber{i}#{empStr}{diskDriveInfo.SerialNumber}");
+                    logStr.AppendLine($"{empStr}#Size{i}#{empStr}{diskDriveInfo.Size}GB");
+                    logStr.AppendLine($"{empStr}#UsedSpace{i}#{empStr}{diskDriveInfo.UsedSpace}GB");
+                    logStr.AppendLine($"{empStr}#FreeSpace{i}#{empStr}{diskDriveInfo.FreeSpace}GB");
+                }
+            }
+            logStr.AppendLine($"{headContentStr}PCInfo{headContentStr}");
+            return logStr.ToString();
+        }
+
+        /// <summary>
+        /// 记录到文件,已存在则追加
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="logMsg"></param>
+        public static void WriteToFile(string path, string logMsg)
+        {
+            using (var sw = new StreamWriter(path, true, Encoding.UTF8))
+            {
+                sw.WriteLine(logMsg);
+                sw.Flush();
+            }
+        }
+     
+        /// <summary>
+        /// 获取log日志目录
+        /// </summary>
+        /// <returns></returns>
+        public static string GetLogDir(string operationLog)
+        {
+            var toolpars = MyTools.Toolpars;
+            var varAppPath = PathTools.PathCombine(toolpars.MvsToolpath, "log");
+            if (!Directory.Exists(varAppPath))
+                Directory.CreateDirectory(varAppPath);
+            var logPath = $@"{varAppPath}\\{operationLog}.log";
+            return logPath;
+        }
+
+        /// <summary>
+        /// 生成一段指定长度的字符
+        /// </summary>
+        /// <param name="targetStr"></param>
+        /// <param name="len"></param>
+        /// <returns></returns>
+        public static string GetHeadStr(string targetStr, int len)
+        {
+            var headStr = string.Empty;
+            for (var i = 0; i <= len; i++)
+                headStr += targetStr;
+            return headStr;
+        }
+      
+        /// <summary>
+        /// 记录到Server
+        /// </summary>
+        /// <param name="fileInfos"></param>
+        public static void WriteToServer(IEnumerable<FileInfos> fileInfos) {
+            var toolpars = MyTools.Toolpars;
+            SqlTools.InsertToolInfo(toolpars.FormEntity.TxtNewTypeKey, fileInfos);
+        }
+
+        #endregion
+    }
+}
